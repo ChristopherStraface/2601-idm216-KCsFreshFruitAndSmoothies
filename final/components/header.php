5b9f6936b2ac5182b6ab8b4d0f50764def65914b
Tom Chen <tc3363@drexel.edu>
2026-03-02 12:43:13 -0500

    Separate header and footer

---
+++ final/components/header.php
@@ -0,0 +1,18 @@
+<header class="header">
+    <div class="header-content">
+        <a href="index.php"><img src="../images/KC’s.svg" alt="KC's Smoothie Bar" class="logo"></a>
+        <div class="header-actions">
+            <a href="bag.php" class="icon-btn" aria-label="View bag">
+                <svg class="icon" viewBox="0 0 32 32" fill="none">
+                    <path d="M25.3333 9.33333H22.6667V8C22.6667 4.68629 19.9804 2 16.6667 2H15.3333C12.0196 2 9.33333 4.68629 9.33333 8V9.33333H6.66667C5.19391 9.33333 4 10.5272 4 12V26.6667C4 28.1394 5.19391 29.3333 6.66667 29.3333H25.3333C26.8061 29.3333 28 28.1394 28 26.6667V12C28 10.5272 26.8061 9.33333 25.3333 9.33333ZM12 8C12 6.15905 13.4924 4.66667 15.3333 4.66667H16.6667C18.5076 4.66667 20 6.15905 20 8V9.33333H12V8Z" fill="#1A1A1A"/>
+                </svg>
+            </a>
+            <button class="icon-btn" aria-label="Profile">
+                <svg class="icon" viewBox="0 0 32 32" fill="none">
+                    <path d="M16 4C12.6863 4 10 6.68629 10 10C10 13.3137 12.6863 16 16 16C19.3137 16 22 13.3137 22 10C22 6.68629 19.3137 4 16 4ZM7.33333 24C7.33333 20.3181 10.3181 17.3333 14 17.3333H18C21.6819 17.3333 24.6667 20.3181 24.6667 24V26.6667C24.6667 27.403 24.0697 28 23.3333 28H8.66667C7.93029 28 7.33333 27.403 7.33333 26.6667V24Z" fill="#1A1A1A"/>
+                </svg>
+            </button>
+        </div>
+    </div>
+    <div class="header-border"></div>
+</header>
